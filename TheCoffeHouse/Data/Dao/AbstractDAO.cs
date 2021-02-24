4b074824be492c8ec9764aa0359ee3e8e2e06b91
vanh55 <truongvietanh221@gmail.com>
2021-02-24 21:23:04 +0700

    fix layout

---
+++ TheCoffeHouse/Data/Dao/AbstractDAO.cs
@@ -0,0 +1,19 @@
+﻿using Data.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Dao
+{
+    public class AbstractDAO
+    {
+        private WebDbContext context = null;
+
+        public AbstractDAO()
+        {
+            context = new WebDbContext();
+        }
+    }
+}
